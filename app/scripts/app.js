f99f6d8b757a9dc0cbb1a31e23e6480c1e035c7d
George Al <gfa61.ga@gmail.com>
2018-04-21 22:56:37 +0300

    feat: Add quiz solution

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,4 +1,4 @@
-/*
+/* // Teacher's solution
 Challenge:
 
 Use what you've learned about Promises to request thumbnails in parallel but create them in the
@@ -22,15 +22,19 @@ proper order even if all the requests haven't finished.
   }
 
   /**
-   * Helper function to create a planet thumbnail.
+   * Helper function to create a planet thumbnail - Promisified version!
    * @param  {Object} data - The raw data describing the planet.
    */
   function createPlanetThumb(data) {
-    var pT = document.createElement('planet-thumb');
-    for (var d in data) {
-      pT[d] = data[d];
-    }
-    home.appendChild(pT);
+    return new Promise(function(resolve) {
+      var pT = document.createElement('planet-thumb');
+      for (var d in data) {
+        pT[d] = data[d];
+      }
+      home.appendChild(pT);
+      console.log('rendered: ' + data.pl_name + Date.now());
+      resolve();
+    });
   }
 
   /**
@@ -43,13 +47,122 @@ proper order even if all the requests haven't finished.
   }
 
   /**
-   * Performs an XHR for a JSON and returns a parsed JSON response.
+   * Performs an XHR for a JSON and returns a parsed JSON response - with a delay!
    * @param  {String} url - The JSON URL to fetch.
    * @return {Promise}    - A promise that passes the parsed JSON response.
    */
   function getJSON(url) {
+    console.log('sent: ' + url + Date.now());
     return get(url).then(function(response) {
-      return response.json();
+      // For testing purposes, I'm making sure that the urls don't return in order
+      if (url === 'data/planets/Kepler-186f.json') {
+        return new Promise(function(resolve) {
+          setTimeout(function() {
+            console.log('received: ' + url + Date.now());
+            resolve(response.json());
+          }, 500);
+        });
+      } else {
+        console.log('received: ' + url + Date.now());
+        return response.json();
+      }
+    });
+  }
+
+  window.addEventListener('WebComponentsReady', function() {
+    home = document.querySelector('section[data-route="home"]');
+
+    getJSON('../data/earth-like-results.json')
+    .then(function(response) {
+      addSearchHeader(response.query);
+      return response;
+    })
+    .then(function(response) {
+      var sequence = Promise.resolve();
+
+      // .map executes all of the network requests immediately.
+      var arrayOfExecutingPromises = response.results.map(function(result) {
+        return getJSON(result);
+      });
+
+      arrayOfExecutingPromises.forEach(function (request) {
+        // Loop through the pending requests that were returned by .map (and are in order) and
+        // turn them into a sequence.
+        // request is a getJSON() that's currently executing.
+        sequence = sequence.then(function() {
+          // Remember that createPlanetThumb is a Promise, so it must resolve before Promises
+          // later in the sequence can execute.
+          return request.then(createPlanetThumb);
+        });
+      });
+    });
+  });
+})(document);
+
+
+
+
+
+/* My solution
+
+(function(document) {
+  'use strict';
+
+  var home = null;
+
+
+  // * Helper function to show the search query.
+  // * @param {String} query - The search query.
+
+  function addSearchHeader(query) {
+    home.innerHTML = '<h2 class="page-title">query: ' + query + '</h2>';
+  }
+
+  //
+  // * Helper function to create a planet thumbnail.
+  // * @param  {Object} data - The raw data describing the planet.
+
+  function createPlanetThumb(data) {
+    return new Promise(function(resolve, reject) {
+      var pT = document.createElement('planet-thumb');
+      for (var d in data) {
+        pT[d] = data[d];
+      }
+      home.appendChild(pT);
+            console.log('rendered: ' + data.pl_name + Date.now());
+      resolve();
+    });
+  }
+
+  //
+  // * XHR wrapped in a promise
+  // * @param  {String} url - The URL to fetch.
+  // * @return {Promise}    - A Promise that resolves when the XHR succeeds and fails otherwise.
+
+  function get(url) {
+    return fetch(url);
+  }
+
+  //**
+  // * Performs an XHR for a JSON and returns a parsed JSON response.
+  // * @param  {String} url - The JSON URL to fetch.
+  // * @return {Promise}    - A promise that passes the parsed JSON response.
+
+  function getJSON(url) {
+    console.log('sent: ' + url+ Date.now());
+    return get(url).then(function(response) {
+      // For testing purposes, I'm making sure that the urls don't return in order
+      if (url === 'data/planets/Kepler-186f.json') {
+        return new Promise(function(resolve) {
+          setTimeout(function() {
+            console.log('received: ' + url + Date.now());
+            resolve(response.json());
+          }, 500);
+        });
+      } else {
+        console.log('received: ' + url+ Date.now());
+        return response.json();
+      }
     });
   }
 
@@ -65,9 +178,15 @@ proper order even if all the requests haven't finished.
         })
       );
     }).then(function(results) {
+
+      var sequence = Promise.resolve();
         results.forEach(function(data) {
-          createPlanetThumb(data);
+          sequence = sequence.then(function() {
+            return createPlanetThumb(data);
+          });
         });
     });
   });
 })(document);
+
+*/
