ab951c8ae84bfee4bf4543779dcc291d4eb577c2
George Al <gfa61.ga@gmail.com>
2018-04-21 17:39:17 +0300

    feat: Add quiz solution

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -59,6 +59,13 @@ Instructions:
     Your code goes here! Uncomment the next line when you're ready to start!
      */
 
-    // getJSON('../data/earth-like-results.json')
+    getJSON('../data/earth-like-results.json').then(function(jsonResponse) {
+      addSearchHeader(jsonResponse.query);
+      jsonResponse.results.map(function(url) {
+        getJSON(url).then(createPlanetThumb);
+      });
+    }).catch(function(error) {
+        console.log(error);
+    });
   });
 })(document);
